88eda6df8407713fab6807662164c9a4477e20ae
unknown <sjyamel1@gmail.com>
2024-10-28 07:37:38 -0700

    Set Background color to white

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ backgroundColor: "white"}}
+      <body style={{ backgroundColor: "white", color: "black" }}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
